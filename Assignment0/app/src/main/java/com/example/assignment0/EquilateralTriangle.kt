920fc411f02549c8eaa97608d1703e458734f0e1
jay newman <tup16252@temple.edu>
2022-09-05 14:04:26 -0400

    Most of the requirements completed. Am I printing in main how prof wants with appropriate headings? Do I have to smart Cast in the link in pdf?

--- Assignment0/app/src/main/java/com/example/assignment0/EquilateralTriangle.kt
+++ Assignment0/app/src/main/java/com/example/assignment0/EquilateralTriangle.kt
@@ -1,7 +1,26 @@
 package com.example.assignment0
 
+import java.util.*
+import kotlin.math.sqrt
+
 class EquilateralTriangle(_name: String) : Triangle(_name) {
+
+    private var side: Double = 0.00
+    private var s: Double = 0.00
+
     override fun setDimensions() {
-        super.setDimensions()
+        print("Please enter the length for all three sides of the triangle and press enter: ")
+        val reader = Scanner(System.`in`)
+        side = reader.nextDouble()
+    }
+
+    override fun getArea(): Double {
+        s = (.5 * (side * 3))
+        return (sqrt(s * (s-side) * (s-side) * (s-side)))
+    }
+
+    override fun printDimensions() {
+        println("The dimensions of the triangle are:\nSide One: $side\nSide Two: $side\n" +
+                "Side Three: $side")
     }
 }
